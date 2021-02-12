df0138bed42574f76d5b3d151290e070fb2b97b3
robertmoore40 <robertcmoore92@gmail.com>
2021-02-12 08:26:12 -0500

    added API call

--- src/Stock.js
+++ src/Stock.js
@@ -19,3 +19,5 @@ componentDidMount() {
     console.log(pointerToThis);
     const API_KEY = 'HGJWFG4N8AQ66ICD';
     let StockSymbol = 'FB';
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}`;
+    let stockChartXValuesFunction = [];
