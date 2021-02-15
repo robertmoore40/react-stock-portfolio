3a721cb98848380bf2e3cf0bc878136b1f18c9b0
robertmoore40 <robertcmoore92@gmail.com>
2021-02-15 09:46:59 -0500

    added return

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -31,3 +31,5 @@ const isLocalhost = Boolean(
               // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
