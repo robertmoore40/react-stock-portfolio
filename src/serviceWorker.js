a7a859a812698b0f80f497c1b9ebfd415fe3ebfd
robertmoore40 <robertcmoore92@gmail.com>
2021-02-15 09:53:50 -0500

    added commits

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -68,3 +68,7 @@ function registerValidSW(swUrl, config) {
           installingWorker.onstatechange = () => {
             if (installingWorker.state === 'installed') {
                 if (navigator.serviceWorker.controller) {
+
+                      // At this point, the updated precached content has been fetched,
+              // but the previous service worker will still serve the older
+              // content until all client tabs are closed.
