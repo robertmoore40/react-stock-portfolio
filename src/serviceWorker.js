28489152e147ca26c346d9044e545f72ca0066f0
robertmoore40 <robertcmoore92@gmail.com>
2021-02-15 09:53:35 -0500

    added additional logic

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -67,3 +67,4 @@ function registerValidSW(swUrl, config) {
           }
           installingWorker.onstatechange = () => {
             if (installingWorker.state === 'installed') {
+                if (navigator.serviceWorker.controller) {
