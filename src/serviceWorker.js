21ba3d5a4b1d3267dc569fc5cb3e33141adcc34f
robertmoore40 <robertcmoore92@gmail.com>
2021-02-15 09:53:06 -0500

    added worker install

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -61,3 +61,7 @@ function registerValidSW(swUrl, config) {
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
+
+        if (installingWorker == null) {
+            return;
+          }
