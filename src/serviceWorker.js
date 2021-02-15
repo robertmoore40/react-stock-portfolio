d8ab8737c44ae8688bfa993e1bef7420aa7d2f0b
robertmoore40 <robertcmoore92@gmail.com>
2021-02-15 09:45:15 -0500

    added comment

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -23,3 +23,4 @@ const isLocalhost = Boolean(
   
   export function register(config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+          // The URL constructor is available in all browsers that support SW.
